c84086cdd6b4ebf6f8506c34c3081e9fb8438705
kponire <kponire@gmail.com>
2024-01-04 08:15:17 -0800

    0x08

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle:
         """instatiation"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
