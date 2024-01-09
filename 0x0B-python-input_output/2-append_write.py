c7717b66d694dedabdebabb174b90967d74f4b6b
kponire <kponire@gmail.com>
2024-01-09 08:26:44 -0800

    0x0B

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,7 @@ Contains the function "wrtie_file"
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ overrides the content of the file with the text """
     with open(filename, 'a', encoding='utf=8') as f:
         return f.write(text)
